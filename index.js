8d722ec72aa593a301bcb489c96c36e00062524e
Zoltan Feher <zkiiito@gmail.com>
2015-12-08 22:02:48 +0100

    ready for github

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express'),
+var fs = require('fs'),
+    express = require('express'),
     cons = require('consolidate'),
     multipart = require('connect-multiparty'),
-    multipartMiddleware = multipart(),
-    app = express(),
+    cloudinary = require('cloudinary'),
     config = require('./src/Config'),
-    redis = require('./src/RedisClient');
-
+    redis = require('./src/RedisClient'),
+    multipartMiddleware = multipart(),
+    app = express();
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
@@ -13,8 +14,6 @@ app.set('views', __dirname);
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/font', express.static(__dirname + '/font'));
-
-
 
 app.get('/:counter', function (req, res) {
     redis.get('counter-' + req.params.counter, function (err, counterValues) {
@@ -33,11 +32,7 @@ app.get('/:counter', function (req, res) {
 
 app.get('/:counter/edit', function (req, res) {
     redis.get('counter-' + req.params.counter, function (err, counterValues) {
-        if (null === counterValues) {
-            counterValues = {};
-        } else {
-            counterValues = JSON.parse(counterValues);
-        }
+        counterValues = null === counterValues ? {} : JSON.parse(counterValues);
 
         res.render('index', {
             title: counterValues.title,
@@ -47,21 +42,32 @@ app.get('/:counter/edit', function (req, res) {
     });
 });
 
-
 app.post('/:counter/edit', multipartMiddleware, function (req, res) {
     var counterValues = req.body.counterValues;
     var password = req.body.password;
 
     if (password == config.adminPass) {
-        if (req.files.imgfile) {
-            //TODO upload
-        }
+        redis.get('counter-' + req.params.counter, function (err, counterValuesRedis) {
+            if (req.files.imgfile && req.files.imgfile.size > 0) {
+                var stream = cloudinary.uploader.upload_stream(function (result) {
+                    counterValues.fakeimgfile = result.url;
+                    redis.set('counter-' + req.params.counter, JSON.stringify(counterValues));
+                    res.send(JSON.stringify(1));
+                });
 
-        redis.set('counter-' + req.params.counter, JSON.stringify(counterValues));
-        return res.send(JSON.stringify(1));
+                fs.createReadStream(req.files.imgfile.path).pipe(stream);
+            } else {
+                counterValuesRedis = null === counterValuesRedis ? {} : JSON.parse(counterValuesRedis);
+
+                counterValues.fakeimgfile = counterValuesRedis.fakeimgfile;
+                redis.set('counter-' + req.params.counter, JSON.stringify(counterValues));
+                return res.send(JSON.stringify(1));
+            }
+        });
+    } else {
+        res.send(JSON.stringify(0));
     }
-    res.send(JSON.stringify(0));
 });
 
-app.listen(3000);
-console.log('Express server listening on port 3000');
+app.listen(config.port);
+console.log('Express server listening on port ' + config.port);
